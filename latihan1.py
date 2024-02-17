d6c38a4d54bf334f26d7670d412c36200e3f3c3a
alifa <syafaalifazahra@gmail.com>
2024-02-16 23:10:15 -0800

    v1

---
+++ latihan1.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import pandas as pd
+import matplotlib
+df = pd.DataFrame({
+  'number': [1, 2, 3, 4, 5],
+  'name': ['ali','ceros','timbul','esok','mayer'],
+  'age': [19, 92, 67, 35, 16],
+  'book':['bumi series', 'aldebaran', 'hello', 'hujan', 'failure tale']
+})
+
+df
