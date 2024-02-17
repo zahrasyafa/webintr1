71203d223d36135bf45450736d0e13779e2c2322
alifa <syafaalifazahra@gmail.com>
2024-02-17 00:56:22 -0800

    v

--- latihan1.py
+++ latihan1.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
-import matplotlib
+
+st.write("Coding Club 2024")
 df = pd.DataFrame({
   'number': [1, 2, 3, 4, 5],
   'name': ['ali','ceros','timbul','esok','mayer'],
