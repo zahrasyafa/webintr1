6b7800b7e115c45c7fdeca231fdb23c686fbeb39
alifa <syafaalifazahra@gmail.com>
2024-03-01 19:52:18 -0800

    ubah

--- page1.py
+++ page1.py
@@ -2,3 +2,6 @@ import streamlit as st
 def page_1():
   st.title("Page 1")
   st.write("This page use to introduction")
+  st.write("Project coding ini menampilkan :")
+  st.write("1) Favorite video!")
+  st.write("2) Code buat aplikasi hitung segitiga :D")
