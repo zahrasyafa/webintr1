35c7af2ce703573f4122cf82391b96f1d3c4f7c9
alifa <syafaalifazahra@gmail.com>
2024-03-01 19:48:50 -0800

    mood

--- page3.py
+++ page3.py
@@ -2,7 +2,7 @@ import streamlit as st
 def page_3():
   st.title("Page 3")
   st.write("This page use to show rumus matematika")
-  st.write("Show halaman dari file.MD (Mark Down)")
+  st.write("Menampilkan halaman dari file.MD (Mark Down)")
 
   with open("alifa.md", "r") as file:
     isi_teks = file.read()
