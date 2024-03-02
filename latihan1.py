66b906916fd5c73edac1b594ab54b25bbbfa39c9
alifa <syafaalifazahra@gmail.com>
2024-03-01 18:10:51 -0800

    modif

--- latihan1.py
+++ latihan1.py
@@ -1,12 +1,34 @@
 import streamlit as st
-import pandas as pd
+# import pandas as pd
 
-st.write("Favorite Character!")
-df = pd.DataFrame({
-  'number': [1, 2, 3, 4, 5],
-  'name': ['ali','ceros','timbul','esok','mayer'],
-  'age': [19, 92, 67, 35, 16],
-  'book':['bumi series', 'aldebaran', 'hello', 'hujan', 'failure tale']
-})
+# st.write("Favorite Character!")
+# df = pd.DataFrame({
+#   'number': [1, 2, 3, 4, 5],
+#   'name': ['ali','ceros','timbul','esok','mayer'],
+#   'age': [19, 92, 67, 35, 16],
+#   'book':['bumi series', 'aldebaran', 'hello', 'hujan', 'failure tale']
+# })
 
-df
+# df
+
+
+def page_1():
+  st.title("Page 1")
+  st.write("This page use to introduction")
+def page_2():
+  st.title("Page 2")
+  st.write("This page use to youtube video")
+def page_3():
+  st.title("Page 3")
+  st.write("This page use to show rumus matematika")
+
+PAGES = {
+  "Page 1" : page_1,
+  "Page 2" : page_2,
+  "Page 3" : page_3,
+}
+
+page = st.sidebar.radio("page", list(PAGES.keys()))
+PAGES[page]()
+
+ 
