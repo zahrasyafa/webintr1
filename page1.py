01e3ada6875c285a427836172fb83b9917e230d4
alifa <syafaalifazahra@gmail.com>
2024-03-08 16:35:34 -0800

    fsg

--- page1.py
+++ page1.py
@@ -1,7 +1,6 @@
 import streamlit as st
 def page_1():
-  st.title("Page 1")
-  st.write("Hai world! Welcome to my website ꒒ ০ ⌵ ୧ ♡")
+  st.title("Hai world! Welcome to my website ꒒ ০ ⌵ ୧ ♡")
   st.write("Syaf, She/Her, 16, Pink addict, Sleepy everytime, Loves read novel and watch k-drama!, Ice cream and salad lover, YUK MAKAN SAYUR (๑ᵔ⤙ᵔ๑)🍽️")
   st.image("Cute Love Cartoons.jpg",width=700)
   
